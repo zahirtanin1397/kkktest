f0d82a22e4f7a783c60f125104908cdaadb56866
zahirtanin1397 <a.zahirtanin786@gmail.com>
2024-01-22 12:37:33 +0430

    fix

--- App.js
+++ App.js
@@ -1,45 +1,14 @@
-import '@azure/core-asynciterator-polyfill';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { DataStore } from 'aws-amplify/datastore';
-import { Amplify } from 'aws-amplify';
-import { useEffect, useState } from 'react';
-import amplifyconfig from './src/amplifyconfiguration.json';
-import {Todo} from "./src/models";
-Amplify.configure(amplifyconfig);
-export default function App() {
-  const [todos, setTodos] = useState([]);
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-  useEffect(() => {
-    fetchTodos();
-  }, []);
-
-  async function fetchTodos() {
-    try {
-      const todos = await DataStore.query(Todo);
-      setTodos(todos);
-    } catch (error) {
-      console.error('Error fetching todos:', error);
-    }
-  }
-
- 
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      {todos.map((todo) => (
-        <Text key={todo.id}>{todo.name}</Text>
-      ))}
+    <View>
+      <Text>App</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+
+const styles = StyleSheet.create({})
